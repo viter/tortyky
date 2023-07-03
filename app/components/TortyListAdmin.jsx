bb3791634d61b07adea9f88e182817d1d976fdb5
Petro Boiko <chekhiv@gmail.com>
2023-07-03 15:29:48 +0300

    fixing hydration failed issue

--- app/components/TortyListAdmin.jsx
+++ app/components/TortyListAdmin.jsx
@@ -3,16 +3,19 @@
 import { setStartupTorty } from '@/slices/tortySlice';
 import { useDispatch, useSelector } from 'react-redux';
 import TortCardAdmin from './TortCardAdmin';
+import { useEffect } from 'react';
 
 export default function TortyListAdmin({ startupTorty }) {
   const torty = useSelector((state) => state.torty.torty);
   const dispatch = useDispatch();
 
-  if (torty.length > 0) {
-    dispatch(setStartupTorty(torty));
-  } else {
-    dispatch(setStartupTorty(startupTorty));
-  }
+  useEffect(() => {
+    if (torty.length > 0) {
+      dispatch(setStartupTorty(torty));
+    } else {
+      dispatch(setStartupTorty(startupTorty));
+    }
+  }, []);
 
   return (
     <>
