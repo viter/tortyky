4bbbc0a73d17a4a6bc21a1e4646c46a563bd9c3f
Petro Boiko <chekhiv@gmail.com>
2023-07-05 13:37:44 +0300

    start adding mobile nav bar

--- app/admin/layout.js
+++ app/admin/layout.js
@@ -1,10 +1,22 @@
+'use client';
+
 import NavBar from '../components/NavBar';
+import Hamburger from '../components/Hamburger';
 import Link from 'next/link';
 import styles from './admin.module.css';
+import MobileNavBar from '../components/MobileNavBar';
+import { useAutoAnimate } from '@formkit/auto-animate/react';
+import { useState } from 'react';
 
 export default function AdminLayout({ children }) {
+  const [parent, enableAnimations] = useAutoAnimate();
+  const [mobileNavBarShown, setMobileNavBarShown] = useState(false);
+
+  function toggleMobileNavBar() {
+    setMobileNavBarShown(!mobileNavBarShown);
+  }
   return (
-    <>
+    <div ref={parent}>
       <nav className={styles.navbar}>
         <div className={styles.logo}>
           <Link href={'/admin'} className={styles.mainLink}>
@@ -12,9 +24,10 @@ export default function AdminLayout({ children }) {
           </Link>
         </div>
         <NavBar />
+        <Hamburger toggleMobileNavBar={toggleMobileNavBar} />
       </nav>
-
+      {mobileNavBarShown ? <MobileNavBar /> : null}
       <div className={styles.main}>{children}</div>
-    </>
+    </div>
   );
 }
---
+++ app/components/MobileNavBar.jsx
@@ -0,0 +1,7 @@
+'use client';
+
+import styles from '../admin/admin.module.css';
+
+export default function MobileNavBar() {
+  return <div className={styles.mobileNavBar}>Hello world!</div>;
+}
